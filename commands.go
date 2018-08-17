89c8fe391cefd9116b77f05e444f7134b063cca3
Giovanni Sakti <giosakti@gmail.com>
2018-08-17 16:38:56 +0700

    [gio] add graceful shutdown for agent

--- commands.go
+++ commands.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/BaritoLog/go-boilerplate/srvkit"
 	"github.com/giosakti/pathfinder-agent/agent"
 	"github.com/urfave/cli"
 )
 
 func CmdAgent(ctx *cli.Context) {
+	fmt.Println("Agent starting...")
 	go runAgent()
+	srvkit.GracefullShutdown(func() {
+		fmt.Println("Agent stopping...")
+	})
 }
 
 func runAgent() {
