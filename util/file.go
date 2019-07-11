06ec9fcdc2f0647ff289e8b820be554114ea8e20
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2019-07-11 15:47:34 +0700

    refactor file based on pr revision
    Co-authored-by: tarabaskara <tara.baskara@gmail.com>"

--- util/file.go
+++ util/file.go
@@ -1,11 +1,14 @@
 package util
 
 import (
+	"fmt"
 	"io"
 	"os"
+
+	"github.com/pathfinder-cm/pathfinder-go-client/pfmodel"
 )
 
-func WriteToFile(filename string, data string) (*os.File, error) {
+func WriteStringToFile(filename string, data string) (*os.File, error) {
 	file, err := os.Create(filename)
 	if err != nil {
 		return file, err
@@ -17,4 +20,25 @@ func WriteToFile(filename string, data string) (*os.File, error) {
 		return file, err
 	}
 	return file, nil
+}
+
+func SetupBootstrapFileContent(bs pfmodel.Bootstrapper, content string, mode int) (string, int) {
+	switch bs.Type {
+	case "chef-solo":
+		content = `
+cd /tmp && curl -LO https://www.chef.io/chef/install.sh && sudo bash ./install.sh -v 14.12.3 && rm install.sh
+cat > solo.rb << EOF
+root = File.absolute_path(File.dirname(__FILE__))
+cookbook_path root + "/cookbooks"
+EOF
+`
+		execChefSoloCmd := fmt.Sprintf("chef-solo -c ~/tmp/solo.rb -j %s %s", bs.Attributes, bs.CookbooksUrl)
+		content = content + "\n" + execChefSoloCmd
+		mode = 600
+	default:
+		content = content
+		mode = mode
+	}
+
+	return content, mode
 }
